71dfbd680943553e9779c8b05b7b51a8730eb929
Kareem Abdel-Muttalib <166181698+Kareem-Abdelmotalib@users.noreply.github.com>
2025-05-01 17:16:20 +0300

    usersMocks.ts

--- apps/app/src/mocks/usersMocks.ts
+++ apps/app/src/mocks/usersMocks.ts
@@ -1,10 +1,15 @@
-import { faker } from "@faker-js/faker";
-
 export default {
   getUsers() {
-    return [...Array(100)].map(() => ({
-      id: faker.string.uuid(),
-      name: `${faker.person.firstName()} ${faker.person.lastName()}`
+    const names = [
+      "Ahmed Tarek", "Mariam Hossam", "Youssef Gamal", "Salma Adel", "Omar Sherif",
+      "Nourhan Mostafa", "Khaled Mahmoud", "Esraa Saeed", "Mohamed Yassin", "Farah Sameh",
+      "Tamer Hany", "Aya Khaled", "Hussein Ali", "Reem Fathy", "Ziad Amr",
+      "Laila Ahmed", "Mostafa Ehab", "Habiba Nour", "Abdelrahman Walid", "Dina Adel"
+    ];
+
+    return names.map((name, index) => ({
+      id: `user-${index + 1}`,
+      name
     }));
   }
 };
