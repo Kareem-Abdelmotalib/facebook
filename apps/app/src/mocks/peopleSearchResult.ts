e843c47585aaec81288a60eb86bf344f00d48a66
Kareem Abdel-Muttalib <166181698+Kareem-Abdelmotalib@users.noreply.github.com>
2025-05-01 17:12:54 +0300

    peopleSearchResult.ts

--- apps/app/src/mocks/peopleSearchResult.ts
+++ apps/app/src/mocks/peopleSearchResult.ts
@@ -1,13 +1,38 @@
-import { faker } from "@faker-js/faker";
-
 export default {
   getPeople() {
-    return [...Array(30)].map((e, i) => ({
-      fullName: `${faker.person.firstName()} ${faker.person.lastName()}`,
-      subtitle: `${i + 20}friends`,
-      description:
-        "Owner and Founder at HELLO Ma Khaing Ma ChoUniversity of Yangon Main",
-      isFriend: false
-    }));
+    const people = [
+      {
+        fullName: "Ahmed Tarek",
+        subtitle: "20 friends",
+        description: "Software Engineer at Cairo University",
+        isFriend: false
+      },
+      {
+        fullName: "Sara Mostafa",
+        subtitle: "25 friends",
+        description: "Marketing Manager at Alex Business School",
+        isFriend: false
+      },
+      {
+        fullName: "Youssef Gamal",
+        subtitle: "30 friends",
+        description: "Founder at EG Startups - Ain Shams University",
+        isFriend: false
+      },
+      {
+        fullName: "Nourhan Adel",
+        subtitle: "22 friends",
+        description: "Content Creator at Helwan University",
+        isFriend: false
+      },
+      {
+        fullName: "Mariam Hossam",
+        subtitle: "28 friends",
+        description: "Graphic Designer at Future Academy",
+        isFriend: false
+      }
+    ];
+
+    return people;
   }
 };
